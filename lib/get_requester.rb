e10056bf758e4ea5ed20a78ab4fdc386f17fe7b7
Ekow Bentsi-Enchill <ekow.bentsi.enchill@gmail.com>
2021-06-27 19:06:02 +0000

    get data from API

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,23 @@
 # Write your code here
+require 'json'
+require 'pry'
+require 'open-uri'
+require 'net/http'
+
+class GetRequester
+    attr_reader :url
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        parsed_data = JSON.parse(self.get_response_body)
+    end
+end
